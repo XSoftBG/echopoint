3e6734c2c7a20336918cf34cb8a20de56d1b974f
Ivan Nikolchov <ivan@xsoftbg.com>
2012-04-20 18:58:25 +0300

    fixed restartApp - does not delete all the cookies anymore

--- src/client/js/RemoteClient.RestartApp.js
+++ src/client/js/RemoteClient.RestartApp.js
@@ -1,40 +1,35 @@
 /**
  * Command execution peer: Close the browser
  */
-Echo.RemoteClient.CommandExec.RestartApp = Core.extend(Echo.RemoteClient.CommandExec, 
-{
-  $static: 
-  {
+Echo.RemoteClient.CommandExec.RestartApp = Core.extend(Echo.RemoteClient.CommandExec, {
+  $static:  {
     /** @see Echo.RemoteClient.CommandExecProcessor#execute */
-    execute: function(client, commandData) 
-    {
+    execute: function(client, commandData) {
       var cookies = document.cookie.split(";");
       var cookiesToRemove = commandData.cookiesToRemove.split(";");
       var path = window.location.href;	
 
       var start = path.indexOf('/', path.indexOf('://') + 3);
       var end = path.indexOf('/', start + 1);
-      if(end < 0)
+      
+      if(end < 0) {
         end = path.length - 1;
+      }
+      
       var app = path.substring(start, end);
 
-      for(var cookie in cookies)
-      {
-        var name = cookie.split("=")[0];
-        var idx = cookiesToRemove.indexOf(name);
-        alert(name);
-        if(idx >= 0)
-          {
-            alert("removed!");
+      for(var cookie in cookies) {        
+        var name = cookies[cookie].split("=")[0].replace(/^\s+|\s+$/g, '');
+        var idx = cookiesToRemove.indexOf(name);      
+        if(idx >= 0) {
             document.cookie = name + '=a; expires=Monday, 19-Aug-1996 05:00:00 GMT; Path=' + app;
-          }
+        }
       }
       window.location.href = path;
     }
   },
   
-  $load: function()
-  {
+  $load: function() {
     Echo.RemoteClient.CommandExecProcessor.registerPeer("echopoint.command.RestartApp", this);
   }
 });
